e28d55d69b29afe98286bfd0218858dbb07be283
Mackowi <dzierzenga.maciej@gmail.com>
2023-03-13 12:31:32 +0100

    fix for height

--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
 import './styles.css'
 
+const startButton = document.querySelector('.welcome-button');
+
+startButton.addEventListener('click', (e) => {
+    console.log(e.target);
+    clearWelcomeScreen();
+})
+
+const clearWelcomeScreen = () => {
+    let welcomeScreen = document.querySelector('.welcome-screen');
+    welcomeScreen.remove();
+}
+
+const createPregameBoard = () => {
+    
+}
